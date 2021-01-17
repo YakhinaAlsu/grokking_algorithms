1d46acd808cd8f554af3997d442ebdb8b61f9c48
Yakhina Alsu <EvergreenSun@yandex.ru>
2021-01-17 21:52:55 +0300

    change

--- 03_recursion.js
+++ 03_recursion.js
@@ -1,4 +1,5 @@
 // отбратный отсчёт
+/*
 function countdown(i) {
     console.log(i);
     if (i<=0) return;
@@ -6,4 +7,14 @@ function countdown(i) {
     
 }
 countdown(5);
+*/
 
+//факториал
+/*
+function fact(x) {
+if (x === 1) return 1;
+return x*fact(x-1);
+}
+
+console.log(fact(5));
+*/
